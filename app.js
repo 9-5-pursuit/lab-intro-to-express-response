9afa956e2c14b9b2401e6ba4b823c6eba1db33ac
Amina-Moufakkir <aminamouf22@gmail.com>
2023-06-21 22:07:46 -0400

    lab and test completed

---
+++ app.js
@@ -0,0 +1,67 @@
+// DEPENDENCIES
+const express = require('express');
+
+// CONFIGURATION
+const app = express();
+
+// Modules
+const { magic8Responses, generateRandomRes } = require('./getRandom');
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.status(200).send(`Hello, world!`);
+});
+app.get('/terminator', (req, res) => {
+  res.send(`I'll be back`);
+});
+
+app.get('/emeril', (req, res) => {
+  res.send(`Bam!`);
+});
+
+app.get('/steve-mcgarrett', (req, res) => {
+  res.send(`Bam!`);
+});
+
+app.get('/Taylor', (req, res) => {
+  res.send(`Clear eyes, full hearts, can't Lose`);
+});
+
+app.get('/Homer-Simpson', (req, res) => {
+  res.send(`D'Oh`);
+});
+
+app.get('/Bruce-Banner', (req, res) => {
+  res.send(`Don't make me angry!`);
+});
+
+app.get('/JJ-Evans', (req, res) => {
+  res.send(`Dy-no-myte!`);
+});
+
+app.get('/Batman', (req, res) => {
+  res.send(`To the Batmobile!`);
+});
+
+app.get('/Hannibal-Smith', (req, res) => {
+  res.send(`I love it when a plan comes together!`);
+});
+
+app.get('/Fraiser', (req, res) => {
+  res.send(`I'm listening!`);
+});
+
+app.get('/Regis', (req, res) => {
+  res.send(`Is that your final answer?`);
+});
+
+app.get(`/Rosie`, (req, res) => {
+  res.send(`Be right back!`);
+});
+
+app.get('/magic8', (req, res) => {
+  res.send(`<h1>${generateRandomRes(magic8Responses)}</h1>`);
+});
+
+// EXPORT
+module.exports = app;

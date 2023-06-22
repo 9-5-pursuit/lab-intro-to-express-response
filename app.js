22a75f9d36f4b22de960cddca203915f4cbb7a6b
Jennifer <jenniferpeterson@pursuit.org>
2023-06-22 08:23:53 -0400

    quick change

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.get("/terminator", (request, response) => {
   response.send("I'll be back");
   //   response.send("Hasta la vista, baby");
 });
+//received an error message when trying to add a new catchphrase to the previous route because The http protocol is very specific in that there is one response for every request.
+//The error message is telling us that we are trying to set another set of headers after the first one.
 
 const magic8Responses = [
   "It is certain",
@@ -48,9 +50,6 @@ app.get("/magic8", (request, response) => {
   const magic8RandomAnswer = magic8Responses[magic8RandomIndex];
   response.send(`<h1>${magic8RandomAnswer}</h1>`);
 });
-
-//received an error message when trying to add a new catchphrase to the previous route because The http protocol is very specific in that there is one response for every request.
-//The error message is telling us that we are trying to set another set of headers after the first one.
 
 app.get("/Emeril", (request, response) => {
   response.send("Bam!");

f17a76966b606692e6b9d8660b16b38ea460c60d
Irfan Hanjra <ihanjra@gmail.com>
2023-06-22 02:19:08 -0400

    Completed bonus

--- app.js
+++ app.js
@@ -24,6 +24,46 @@ const magic8Responses = [
   "Very doubtful",
 ];
 
+app.get("/terminator", (req, res) => {
+  res.send("I'll be back");
+});
+
+app.get("/Emeril", (req, res) => {
+  res.send("Bam!");
+});
+
+app.get("/pak-chu", (req, res) => {
+  res.send("I like hamsters");
+});
+
+app.get("/Homer-Simpson", (req, res) => {
+  res.send("D'Oh");
+});
+
+app.get("/JJ-Evans", (req, res) => {
+  res.send("Dy-no-myte!");
+});
+
+app.get("/Batman", (req, res) => {
+  res.send("To the Batmobile!");
+});
+
+app.get("/Tony-Montana", (req, res) => {
+  res.send("Say hello to my little friend");
+});
+
+app.get("/Gollum", (req, res) => {
+  res.send("My precious");
+});
+
+app.get("/Rocky", (req, res) => {
+  res.send("Yo, Adrian");
+});
+
+app.get("/Zeus", (req, res) => {
+  res.send("Release the Kraken");
+});
+
 function getRandomAnswer() {
   //Math.random returns a decimal number between 0 and 1
   //Generates a random index using Math.random() multiplied by the length of the magic8Responses array. 
@@ -34,13 +74,13 @@ function getRandomAnswer() {
   return magic8Responses[randomIndex];
 };
 
+app.get("/", (req, res) => {
+  res.status(418).send("Hello, world!");
+});
+
  app.get("/magic8", (req, res) => {
-    const magic8Answer = getRandomAnswer();
-   res.send(`<h1>${magic8Answer}</h1>`);
+   const magic8Answer = getRandomAnswer();
+   res.status(404)(`<h1>${magic8Answer}</h1>`);
  });
 
-const PORT = 3003;
-
-app.listen(PORT, () => {
-  console.log(`Server is now running on port ${PORT}`);
-});
+module.exports = app;
